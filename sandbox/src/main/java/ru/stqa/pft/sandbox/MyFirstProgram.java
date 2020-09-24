4c37b65ca6933893a8605f01af7be44d8f58c715
Inna Makarenko <inna.makarenko@gmail.com>
2020-09-25 00:27:59 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,24 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    hello("Alexey");
+    hello("user");
+    hello("Inna");
+    double l=5;
+    System.out.println("Square for length = " + l + "=" + area(l));
+    double a =4;
+    double b =6;
+    System.out.println("square with sides " + a + "and "+ b + " = " + area(a,b));
+
+  }
+
+  public static void hello(String somebody) {
+        System.out.println("Hello," + somebody);
+  }
+  public static double area(double len){
+    return len*len;
+  }
+  public static double area(double a, double b) {
+    return a*b;
   }
 }
