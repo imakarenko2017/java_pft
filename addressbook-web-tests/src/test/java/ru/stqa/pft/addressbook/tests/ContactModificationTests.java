14f7f9888153602aada5caa22aaec160ec404d53
Inna Makarenko <inna.makarenko@gmail.com>
2020-10-23 15:25:03 +0300

    Добавлена проверка предусловий в тест модификации контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -9,6 +9,9 @@ public class ContactModificationTests extends TestBase {
   public void contactModificationTest() {
 
     app.contactHelper.gotoContactPage();
+    if (!app.contactHelper.isThereAContact()) {
+      app.contactHelper.createContact(new ContactData("Inna", "middlename", "Makarenko", "imakarenko", "Vintelligent,Inc", "1567 Brooklyne MA", "123842", "inna.makarenko@gmail.com", "3", "March", "1980","[none]"));
+    }
     app.contactHelper.editFirstContact();
     app.contactHelper.fillContactForm(new ContactData ("new name","dsfjksdhfk","Makarenko","sdsdf","VT","Brookline","1232131","inna@vintellligent.com","3","March","1980",null),false);
     app.contactHelper.ModifyContactForm();
